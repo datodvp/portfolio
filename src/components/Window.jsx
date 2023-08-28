7b9e4ceb4d5ee6d6a9b134c7a1099e7d254a55b1
datodvp <gaguadato98@gmail.com>
2023-08-28 13:00:09 +0400

    feat: add framer motion and small animations

--- src/components/Window.jsx
+++ src/components/Window.jsx
@@ -1,14 +1,19 @@
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Window = ({ title, children }) => {
   return (
-    <div className="h-full m-auto w-[73%]">
-      <div className="w-full h-full">
-        <div className="absolute top-10 w-[80%] h-[80%] bg-white">
+    <motion.div
+      animate={{ scale: 1 }}
+      initial={{ scale: 0 }}
+      className="h-full m-auto w-[73%] z-10"
+    >
+      <div className="flex items-center justify-center w-full h-full">
+        <div className="w-full h-[80%] bg-white">
           <div className="flex items-center justify-between w-full px-6 h-14 bg-slate-900">
             <h2 className="text-3xl text-white">{title}</h2>
             <Link to="/">
-              <div className="h-[70%] cursor-pointer rounded-xl w-10 bg-red-600 flex justify-center items-center text-white font-black text-3xl">
+              <div className="h-[70%] cursor-pointer rounded-xl w-10 bg-red-600 hover:bg-red-400 flex justify-center items-center text-white font-black text-3xl">
                 X
               </div>
             </Link>
@@ -16,7 +21,7 @@ const Window = ({ title, children }) => {
           <section>{children}</section>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
