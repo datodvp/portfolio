ec724475cd0c8bb3449afd65d7f7a545f913e887
datodvp <gaguadato98@gmail.com>
2023-09-01 00:00:53 +0400

    feat: card and window responsive

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -3,10 +3,14 @@ const Card = ({ image, text, isChosen }) => {
     <div
       className={`${
         isChosen && 'bg-primary-orange'
-      } w-[340px] h-[300px] flex flex-col justify-center items-center gap-2 bg-primary-green hover:scale-90 duration-200 hover:shadow-2xl hover:border-orange-400 cursor-pointer border-primary-orange border-2 rounded-[3rem] `}
+      } w-[200px] h-[150px] md:w-[340px] md:h-[300px] flex flex-col justify-center items-center gap-2 bg-primary-green hover:scale-90 duration-200 hover:shadow-2xl hover:border-orange-400 cursor-pointer border-primary-orange border-2 rounded-[3rem] `}
     >
-      <img src={image} alt="projects logo" className="w-[10rem] h-[10rem] " />
-      <h3 className="text-3xl font-bold text-white">{text}</h3>
+      <img
+        src={image}
+        alt="projects logo"
+        className="w-[5rem] md:w-[10rem] h-[5rem] md:h-[10rem] "
+      />
+      <h3 className="text-2xl font-bold text-white">{text}</h3>
     </div>
   );
 };
--- src/components/Window.jsx
+++ src/components/Window.jsx
@@ -6,12 +6,12 @@ const Window = ({ title, children }) => {
     <motion.div
       animate={{ scale: 1 }}
       initial={{ scale: 0 }}
-      className="h-[90%] m-auto w-[73%] z-10"
+      className="h-[90%] m-auto w-[100%] md:w-[73%] z-10"
     >
-      <div className="flex items-center justify-center w-full h-full">
-        <div className="w-full h-[80%] bg-white">
+      <div className="flex justify-center w-full h-full md:items-center">
+        <div className="w-full h-full md:h-[80%] bg-white">
           <div className="flex items-center justify-between w-full px-6 h-[7.5%] bg-slate-900">
-            <h2 className="text-3xl text-white">{title}</h2>
+            <h2 className="text-2xl text-white md:text-3xl">{title}</h2>
             <Link to="/">
               <div className="h-[70%] cursor-pointer rounded-xl w-10 bg-red-600 hover:bg-red-400 flex justify-center items-center text-white font-black text-3xl">
                 X
