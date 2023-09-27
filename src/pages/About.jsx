9f721c4d27c9f19494d8e242e200b9b0bad798c3
datodvp <gaguadato98@gmail.com>
2023-09-27 16:10:13 +0400

    wip: about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,30 @@
 import Window from '../components/Window';
+import ImageMan from '../assets/images/man.png';
+import IconCopy from '../assets/images/copy.svg';
+import ImageGmail from '../assets/images/gmail.png';
 
 const About = () => {
-  return <Window title="About Me">hello</Window>;
+  return (
+    <Window title="About Me">
+      <div className="flex h-full">
+        <div className="w-[30%] flex flex-col items-center mt-5 gap-3">
+          <img
+            src={ImageMan}
+            alt="man"
+            className="w-[50%] border border-black mb-5"
+          />
+          <div className="flex items-center gap-5 p-4 text-lg duration-150 border border-black cursor-pointer hover:bg-slate-300">
+            <img src={ImageGmail} alt="gmail" className="w-8 h-8" />
+
+            <p>datogagua98@gmail.com</p>
+
+            <img src={IconCopy} alt="copy" className="scale-150" />
+          </div>
+        </div>
+        <div className="w-[70%] bg-green-700 "></div>
+      </div>
+    </Window>
+  );
 };
 
 export default About;
