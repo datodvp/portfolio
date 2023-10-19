8dacb0004cba8262031019d9037ab50677acc8e8
datodvp <gaguadato98@gmail.com>
2023-10-19 18:44:36 +0400

    feat: update projects text

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,14 @@
 import Window from '../components/Window';
 
 const Projects = () => {
-  return <Window title="Projects">hello</Window>;
+  return (
+    <Window title="Projects">
+      <h1 className="p-4 text-xl text-center">
+        This Portfolio is Relatively new so im working on projects to fill
+        that...
+      </h1>
+    </Window>
+  );
 };
 
 export default Projects;
