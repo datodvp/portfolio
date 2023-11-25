409de86e42a5fffffb036300f9df5067cf883c25
datodvp <gaguadato98@gmail.com>
2023-11-26 02:44:44 +0400

    feat: add project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -10,80 +10,143 @@ import ImageVue from '../assets/images/vue.png';
 import ImagePhp from '../assets/images/php.png';
 import ImageLaravel from '../assets/images/laravel.svg';
 import IconGithub from '../assets/images/github.svg';
-import MovieQuotes from '../assets/images/projects/movie-quotes.png'
+import MovieQuotes from '../assets/images/projects/movie-quotes.png';
+import IconMovieQuotes from '../assets/images/projects/movie-quotes-icon.ico';
+import IconResumeCreator from '../assets/images/projects/resume-creator-icon.ico';
+import ImageResumeCreator from '../assets/images/projects/resume-creator.png';
 
 const Projects = () => {
-  const data = new Map([
-    ['Movie Quotes', {
-      title: "Movie quotes",
+  const data = [
+    {
+      title: 'Movie Quotes',
       img: MovieQuotes,
       github: {
-        front: "https://github.com/datodvp/movie-quotes-front",
-        back: "https://github.com/datodvp/movie-quotes-back",
+        front: 'https://github.com/datodvp/movie-quotes-front',
+        back: 'https://github.com/datodvp/movie-quotes-back',
       },
-      live: "https://movie-quotes.dato.com.ge",
-      short_desc: "CRUD, Live updates(Pusher), Register, Google Auth, Language Translations",
-      desc: "This is Social Network where you can Post, Like, Comment, Update and Delete. It also updated Likes and Comments Live from different users. You have update User profile picture or Username.",
-      stack: [
-        ImageVue,
-        ImageLaravel,
-        ImageTailwind
-      ]
-    }],
-    ['Ecommerce', {
-      title: "Ecommerce",
-      github: {
-        front: "https://github.com/datodvp/movie-quotes-front",
-        back: "https://github.com/datodvp/movie-quotes-back"
-      },
-      live: "https://dato.com.ge",
-      short_desc: "Ecommerce Project",
-      desc: "In this app you can buy products. Also being admin you can ADD or REMOVE products UPDATE them. There is also Stripe payment",
-      stack: [
-        ImageLaravel,
-        ImageReact,
-      ]
-    }]
-  ])
-  const [chosenProject, setChosenProject] = useState("Movie Quotes");
+      live: 'https://movie-quotes.dato.com.ge',
+      short_desc:
+        'CRUD, Live updates(Pusher), Register, Google Auth, Language Translations',
+      desc: 'This is Social Network where you can Post, Like, Comment, Update and Delete. Website also updates Likes and Comments Live from different users. You can update User profile picture or Username which will be affected everywhere in the app.',
+      stack: [ImageVue, ImageLaravel, ImageTailwind],
+      icon: IconMovieQuotes,
+    },
+    {
+      title: 'Resume Creator',
+      img: ImageResumeCreator,
+      github: 'https://github.com/datodvp/resume-app.git',
+      live: 'https://resume-creator.dato.com.ge',
+      short_desc: 'formik, yup, sass and complex forms',
+      desc: 'Interactive and fun resume creator with annoying validations and complex forms.',
+      stack: [ImageReact, ImageHtml, ImageJavascript],
+      icon: IconResumeCreator,
+    },
+  ];
+
+  const [chosenProject, setChosenProject] = useState(0);
+
   return (
     <Window title="Projects">
       <div className="flex h-full">
-        <div className='flex-[0.3] flex flex-col'>
-          <div onClick={() => {
-            setChosenProject("Movie Quotes")
-          }}
-          className={`cursor-pointer p-2 border-y-[2px] border-black ${chosenProject === "Movie Quotes" ? 'bg-slate-400' : 'hover:bg-slate-200'} `}>Movie Quotes</div>
-          <div onClick={() => {
-            setChosenProject("Ecommerce")
-          }}
-          className={`cursor-pointer p-2 border-b-[2px] border-black ${chosenProject === "Ecommerce" ? 'bg-slate-400 ' : 'hover:bg-slate-200'} `}>Ecommerce</div>
+        <div className="flex-[0.3] flex flex-col">
+          {data.map((item, index) => {
+            return (
+              <div
+                key={index}
+                onClick={() => {
+                  setChosenProject(index);
+                }}
+                className={`cursor-pointer flex items-center gap-2 p-2 border-b-[2px] border-b-black ${
+                  chosenProject === index
+                    ? 'bg-slate-400'
+                    : 'hover:bg-slate-200'
+                } `}
+              >
+                <img
+                  src={item.icon}
+                  width={20}
+                  height={20}
+                  className="object-contain"
+                />{' '}
+                {item.title}
+              </div>
+            );
+          })}
         </div>
-        <div className='flex-1 bg-slate-200 p-3 text-black overflow-auto'>
+        <div className="flex-1 p-3 overflow-auto text-black bg-slate-200">
           <div>
-            <h1 className='text-center text-2xl font-bold mb-1 underline'>{data.get(chosenProject).title}</h1>
-            <div className='flex justify-center gap-2 mb-2'>
-              {data.get(chosenProject).stack.map((item, index) => {
-                return <img src={item} key={index} width={30} height={30}/>
+            <h1 className="mb-1 text-2xl font-bold text-center underline">
+              {data[chosenProject].title}
+            </h1>
+            <div className="flex justify-center gap-2 mb-2">
+              {data[chosenProject].stack.map((item, index) => {
+                return <img src={item} key={index} width={30} height={30} />;
               })}
             </div>
           </div>
-          <div className='flex justify-center'>
-            {data.get(chosenProject).img && <img src={data.get(chosenProject).img} width={300} height={150} alt="" />}
+          <div className="flex justify-center">
+            {data[chosenProject].img && (
+              <img
+                src={data[chosenProject].img}
+                width={300}
+                height={150}
+                className="hover:scale-[2] transition-all rounded-md"
+              />
+            )}
           </div>
-          <p className='mt-3 font-semibold'>{data.get(chosenProject).short_desc}</p>
-          <p className='text-sm mt-3'>{data.get(chosenProject).desc}</p>
+          <p className="mt-3 font-semibold">{data[chosenProject].short_desc}</p>
+          <p className="mt-3 text-sm">{data[chosenProject].desc}</p>
           <div>
-            <div className='w-fit flex gap-2 mt-5'>
+            <div className="flex gap-2 mt-5 w-fit">
               <div>
-                <a href={data.get(chosenProject).live} target='_blank' className='flex gap-2 bg-[#b84262] p-2 hover:bg-red-500 text-white rounded-md'><img src="https://cdn-icons-png.flaticon.com/512/7710/7710466.png" width={25} height={25} />Check Demo</a>
+                <a
+                  href={data[chosenProject].live}
+                  target="_blank"
+                  className="flex gap-2 bg-[#b84262] p-2 hover:bg-red-500 text-white rounded-md"
+                >
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/7710/7710466.png"
+                    width={25}
+                    height={25}
+                  />
+                  Check Demo
+                </a>
               </div>
-              <div>
-                <a href={data.get(chosenProject).github.back} target='_blank' className='flex gap-2 bg-[#42b883] p-2 hover:bg-[#5ecb9a] rounded-md text-white'><img src={IconGithub} width={25} height={25} />Backend</a>
-              </div>
-              <div>
-                <a href={data.get(chosenProject).github.front} target='_blank' className='flex gap-2 bg-[#42b883] p-2 hover:bg-[#5ecb9a]   rounded-md text-white'><img src={IconGithub} width={25} height={25} />FrontEnd</a>
-              </div>
+              {typeof data[chosenProject].github === 'object' ? (
+                <>
+                  <div>
+                    <a
+                      href={data[chosenProject].github.back}
+                      target="_blank"
+                      className="flex gap-2 bg-[#42b883] p-2 hover:bg-[#5ecb9a] rounded-md text-white"
+                    >
+                      <img src={IconGithub} width={25} height={25} />
+                      Backend
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href={data[chosenProject].github.front}
+                      target="_blank"
+                      className="flex gap-2 bg-[#42b883] p-2 hover:bg-[#5ecb9a]   rounded-md text-white"
+                    >
+                      <img src={IconGithub} width={25} height={25} />
+                      FrontEnd
+                    </a>
+                  </div>
+                </>
+              ) : (
+                <div>
+                  <a
+                    href={data[chosenProject].github}
+                    target="_blank"
+                    className="flex gap-2 bg-[#42b883] p-2 hover:bg-[#5ecb9a]   rounded-md text-white"
+                  >
+                    <img src={IconGithub} width={25} height={25} />
+                    Github
+                  </a>
+                </div>
+              )}
             </div>
           </div>
         </div>
